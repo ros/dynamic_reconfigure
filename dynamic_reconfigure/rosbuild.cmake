f6db6326c479851c09a2ad1c345496f50b06ea4d
straszheim@eb33c2ac-9c88-4c90-87e0-44a10359b0c3 <straszheim@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-05-09 21:31:02 +0000

    add dependencies so that things are built in the correct order: rospy depends on std_msgs codegen having been run.
    --HG--
    extra : convert_revision : svn%3Aeb33c2ac-9c88-4c90-87e0-44a10359b0c3/stacks/driver_common/trunk%4036728

--- dynamic_reconfigure/rosbuild.cmake
+++ dynamic_reconfigure/rosbuild.cmake
@@ -1,6 +1,5 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+
 rosbuild_add_library(dynamic_reconfigure_config_init_mutex src/dynamic_reconfigure_config_init_mutex.cpp)
 add_subdirectory(test)
 
