ed6ca1cae3d29ef2be85563870e52dfaa5cc49a0
Ze'ev Klapow <zklapow@willowgarage.com>
2012-08-15 10:37:56 -0700

    Catkinize.
    --HG--
    rename : catkin.cmake => CMakeLists.txt

--- rosbuild.cmake
+++
@@ -1,5 +0,0 @@
-include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-
-rosbuild_add_library(dynamic_reconfigure_config_init_mutex src/dynamic_reconfigure_config_init_mutex.cpp)
-add_subdirectory(test)
-
