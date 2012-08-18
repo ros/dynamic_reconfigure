2ef41ac8cb6e47e05e9d6f5ae9e78085f05a9ba5
Ze'ev Klapow <zklapow@willowgarage.com>
2012-08-17 18:18:33 -0700

    More catkinization.

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
-
 import sys
+from xml.etree.ElementTree import ElementTree
+
+try:
+    root = ElementTree(None, 'stack.xml')
+    version = root.findtext('version')
+except Exception, e:
+    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+    sys.exit(-1)
+
 sys.path.insert(0, 'src')
 
+PKG = 'dynamic_reconfigure'
+gen = ['msg', 'srv']
+packages = [PKG]
+package_dir = {PKG: 'src/%s'%PKG}
+if 'CATKIN_BINARY_DIR' in os.environ:
+     build_d = os.environ['CATKIN_BINARY_DIR']
+     for t in gen:
+         p = os.path.join(build_d, 'gen', 'py', PKG, t)
+         if os.path.isdir(p):
+             # e.g. std_msgs.msg = build/gen/py/std_msgs/msg
+             package_dir["%s.%s"%(PKG, t)] = p
+             packages.append("%s.%s"%(PKG, t))
+
 setup(name='dynamic_reconfigure',
-      version= '0.0.0',
-      packages=['dynamic_reconfigure'],
-      package_dir = {'':'src'},
-      install_requires=['ros_comm'],
+      version= version,
+      packages= packages, 
+      package_dir = package_dir, 
+      install_requires= ['ros_comm'],
       author = "", 
       author_email = "",
       url = "",
