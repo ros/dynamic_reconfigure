8aa91061dc160155756fbb27767f50f40bedb243
Dirk Thomas <dthomas@willowgarage.com>
2012-12-07 15:07:23 -0800

    update setup() to use generate_distutils_setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from catkin_pkg.package import parse_package_for_distutils
 
-d = parse_package_for_distutils()
-d['packages'] = ['dynamic_reconfigure']
-d['package_dir'] = {'': 'src'}
-d['requires'] = ['roslib', 'rospy', 'rosservice']
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['dynamic_reconfigure'],
+    package_dir={'': 'src'},
+    requires=['roslib', 'rospy', 'rosservice']
+)
 
 setup(**d)
