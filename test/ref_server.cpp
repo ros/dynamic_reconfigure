afa6210d5299b56bc41916dc9c59e52fe833e2eb
Kei Okada <kei.okada@gmail.com>
2014-11-08 02:27:05 +0900

    add test code for unicode input

--- test/ref_server.cpp
+++ test/ref_server.cpp
@@ -34,8 +34,8 @@
 #include <dynamic_reconfigure/TestConfig.h>
 
 void callback(dynamic_reconfigure_test::TestConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure request: %i %f %s %i %i Group1: %i Group2: %f %s",
-            config.int_, config.double_, config.str_.c_str(),
+  ROS_INFO("Reconfigure request: %i %f %s %s %i %i Group1: %i Group2: %f %s",
+            config.int_, config.double_, config.str_.c_str(), config.mstr_.c_str(),
             (int) config.bool_, config.level, config.group1_int,
             config.group2_double, config.group2_string.c_str());
 }
