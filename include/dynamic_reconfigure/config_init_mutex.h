ae4cd30fc375c3b0415ac9110e8e6bbd61c1c6e1
James Xu <kejxu@microsoft.com>
2019-01-24 09:33:42 -0800

    add dll import and export macros

--- include/dynamic_reconfigure/config_init_mutex.h
+++ include/dynamic_reconfigure/config_init_mutex.h
@@ -3,9 +3,23 @@
 
 #include <boost/thread/mutex.hpp>
 
+#include <ros/macros.h>
+
+// Import/export for windows dll's and visibility for gcc shared libraries.
+
+#ifdef ROS_BUILD_SHARED_LIBS // ros is being built around shared libraries
+  #ifdef dynamic_reconfigure_config_init_mutex_EXPORTS // we are building a shared lib/dll
+    #define DYNAMIC_RECONFIGURE_CONFIG_INIT_MUTEX_DECL ROS_HELPER_EXPORT
+  #else // we are using shared lib/dll
+    #define DYNAMIC_RECONFIGURE_CONFIG_INIT_MUTEX_DECL ROS_HELPER_IMPORT
+  #endif
+#else // ros is being built around static libraries
+  #define DYNAMIC_RECONFIGURE_CONFIG_INIT_MUTEX_DECL
+#endif
+
 namespace dynamic_reconfigure
 {
-  extern boost::mutex __init_mutex__;
+  extern DYNAMIC_RECONFIGURE_CONFIG_INIT_MUTEX_DECL boost::mutex __init_mutex__;
 }
 
 #endif
