4321828b454fddfc419abc952a492a5faa7b18b6
Gaël Écorchard <gael.ecorchard@cvut.cz>
2022-02-05 10:02:18 +0100

    Revert unrelated changes
    Signed-off-by: Gaël Écorchard <gael.ecorchard@cvut.cz>

--- src/dynamic_reconfigure/parameter_generator.py
+++ src/dynamic_reconfigure/parameter_generator.py
@@ -41,6 +41,7 @@
 # Need to put sane error on exceptions
 from __future__ import print_function
 
+import roslib; roslib.load_manifest("dynamic_reconfigure")
 import roslib.packages
 from string import Template
 import os
